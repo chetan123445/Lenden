3f96a1213358ff548f101efd5b99035db575c2cc
chetan123445 <2022csb1074@iitrpr.ac.in>
2026-02-17 00:23:29 +0530

    build errors sorted

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -22,7 +22,6 @@ subprojects {
     tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = JavaVersion.VERSION_17.toString()
         targetCompatibility = JavaVersion.VERSION_17.toString()
-        options.release.set(17)
     }
 }
 
